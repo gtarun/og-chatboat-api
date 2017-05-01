2178e25daea4f27bf24e1d46358b32895c109fec
Biplab Malakar <biplab.x@venturepact.com>
2017-05-01 12:50:49 -0700

    cors added

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const messenger = require('../controller/messenger');
+const cors = require('cors');
+
+router.use(cors());
 
 router.get('/messenger/userwebhook', messenger.webhookVerify);
 router.post('/messenger/userwebhook', messenger.webhookPost);
