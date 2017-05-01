15f74c5db43d42447c18260c87ab25dd59dcd920
Biplab Malakar <biplab.x@venturepact.com>
2017-05-01 13:21:33 -0700

    working

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const messenger = require('../controller/messenger');
 const cors = require('cors');
+const bodyparser = require('body-parser');
 
-router.use(cors());
+router.use(bodyparser.json());
+
+//router.use(cors());
 
 router.get('/messenger/userwebhook', messenger.webhookVerify);
 router.post('/messenger/userwebhook', messenger.webhookPost);
